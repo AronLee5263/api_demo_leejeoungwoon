26235c1fa66f078f6dd16291a7f6fa8e68ce3555
AronLee5263 <aronlee5263@gmail.com>
2023-08-18 21:44:46 +0900

    db 요청
    작업
    
    - 타이틀 변경
    - postman에서 http://localhost:3000/database 로  get 요청
    3개의 객체만 들어있는 배열 요청

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+const database = [
+  { id: 1, title: "글1" },
+  { id: 2, title: "글2" },
+  { id: 3, title: "글3" },
+];
+
+app.get("/database", function (req, res) {
   //   res.send("Hello World !!! 시작");
-  res.sendFile(__dirname + "/views/index.html");
+  res.send(database);
 });
 
 app.listen(3000, () => {
