2757a119f4c4bd46636199e15baed72d980b9c88
AronLee5263 <aronlee5263@gmail.com>
2023-08-18 21:31:45 +0900

    문자열 대신 html 파일 보내기

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  //   res.send("Hello World !!! 시작");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(3000, () => {
   console.log("server On !!!!!!");
 });
+
+// http://localhost:3000
