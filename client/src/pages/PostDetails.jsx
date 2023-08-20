ddc6c95602adf918c80adb577febe19ca01ca337
AronLee5263 <aronlee5263@gmail.com>
2023-08-20 22:02:33 +0900

    상세페이지 구현
    useLocation으로 데이터 받기

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -1,22 +1,42 @@
 import classes from "./PostDetails.module.css";
 import axios from "axios";
 
+import { useLocation } from "react-router-dom";
 // import { Outlet } from "react-router-dom";
 
 export default function PostDetails() {
+  const location = useLocation();
+  const { id, title, content, likes } = location.state;
+  //   console.log("location : " + id, title, content, likes);
+
   return (
     <>
       {/* <Outlet /> */}
       <main>
-        <div className={classes.temp}> PostDetails 페이지 구현중</div>
+        <div className={classes.box}>
+          <div className={classes.post}>
+            <div className={classes.top}>
+              <p className={classes.id}> {id}번</p>
+              <p className={classes.title}>{title}</p>
+            </div>
+            <hr />
+            <div className={classes.middle}>
+              <p className={classes.content}>{content}</p>
+            </div>
+            <div className={classes.bottom}>
+              <p className={classes.likes}>좋아요 {likes} 개</p>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
 }
 
 export async function loader({ params }) {
-  const response = await axios.get(SERVER_URL + "/" + params.postId);
+  const response = await axios.get(SERVER_URL + "/" + params.contentId);
   //   console.log("response.data : ", response.data);
 
-  return response.data.post;
+  //   return response.data.post;
+  return response.data;
 }
