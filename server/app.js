8f780aa5f58b61cc362862b18ab378cb80afb79f
AronLee5263 <aronlee5263@gmail.com>
2023-08-20 20:37:44 +0900

    cors  install, import

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql2/promise");
+const cors = require("cors");
 
 // create the connection to database
 let connection;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
