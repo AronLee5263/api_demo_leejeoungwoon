c46d0a9c56141a6648a223322d548be4c258cfb9
AronLee5263 <aronlee5263@gmail.com>
2023-08-20 19:29:58 +0900

    포스팅 페이지 진입을 위해  useNavigate 사용

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import classes from "./App.module.css";
 
 function App() {
+  let navigate = useNavigate();
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className={classes.posts}>앱 시작 하기</div>
+      <div className={classes.box}>
+        <button
+          className={classes.startBtn}
+          onClick={() => {
+            navigate("/library/content");
+          }}
+        >
+          앱 시작 하기
+        </button>
+      </div>
     </>
   );
 }
