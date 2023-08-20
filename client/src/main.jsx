0047c2415dafc7af3553b357ec631a3ce77c2a7a
AronLee5263 <aronlee5263@gmail.com>
2023-08-20 19:15:31 +0900

    loader action 해제

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./pages/App";
-import Library, { loader as libraryLoader } from "./pages/Library";
-import NewPost, { action as newPostAction } from "./pages/NewPost";
-import PostDetails, { loader as postDatailsLoader } from "./pages/PostDetails";
+import Library from "./pages/Library";
+import NewPost from "./pages/NewPost";
+import PostDetails from "./pages/PostDetails";
+// import Library, { loader as libraryLoader } from "./pages/Library";
+// import NewPost, { action as newPostAction } from "./pages/NewPost";
+// import PostDetails, { loader as postDatailsLoader } from "./pages/PostDetails";
 
 import "./index.css";
 
@@ -13,24 +16,21 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "/library/content",
-        element: <Library />,
-        loader: libraryLoader,
-      },
-    ],
   },
-
+  {
+    path: "/library/content",
+    element: <Library />,
+    // loader: libraryLoader,
+  },
   {
     path: "/create-post",
     element: <NewPost />,
-    action: newPostAction,
+    // action: newPostAction,
   },
   {
     path: "/library/content/:contentId",
     element: <PostDetails />,
-    loader: postDatailsLoader,
+    // loader: postDatailsLoader,
   },
 ]);
 
