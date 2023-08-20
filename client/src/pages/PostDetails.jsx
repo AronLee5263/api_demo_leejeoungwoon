f9346cdfc7b172167ef0de192b7574f7c7077044
AronLee5263 <aronlee5263@gmail.com>
2023-08-21 01:42:45 +0900

    상세페이지에서 좋아요 업데이트

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -1,13 +1,28 @@
 import classes from "./PostDetails.module.css";
 import axios from "axios";
 
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 // import { Outlet } from "react-router-dom";
 
 export default function PostDetails() {
   const location = useLocation();
-  const { id, title, content, likes } = location.state;
+  const { id, title, content } = location.state;
+  const [likes, setLikes] = useState(location.state.likes);
   //   console.log("location : " + id, title, content, likes);
+
+  function IncreaseLikeCount(id) {
+    fetch(`http://localhost:3000/library/content/${id}/like`, {
+      method: "PUT",
+    })
+      .then((response) => response.text())
+      .then(() => {
+        setLikes(likes + 1); // likes 상태 변수를 업데이트합니다.
+      })
+      .catch((error) => {
+        console.error("좋아요 업데이트 에러:", error);
+      });
+  }
 
   return (
     <>
@@ -24,6 +39,8 @@ export default function PostDetails() {
               <p className={classes.content}>{content}</p>
             </div>
             <div className={classes.bottom}>
+              <button className={classes.like_button} onClick={() => IncreaseLikeCount(id)}></button>
+
               <p className={classes.likes}>좋아요 {likes} 개</p>
             </div>
           </div>
